29a8ebf0780e9defce3b55ba4459428d922fe03f
Erik Weibust <erikweibust@gmail.com>
2024-08-18 21:55:56 -0500

    still cleaning up project name change

--- src/main/java/com/example/streams/StreamsApplication.java
+++ src/main/java/com/example/streams/StreamsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo2Application {
+public class StreamsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo2Application.class, args);
+		SpringApplication.run(StreamsApplication.class, args);
 	}
 
 }
